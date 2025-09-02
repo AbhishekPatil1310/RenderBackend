f1e341e73b88e76f7336bf2273ac35b3c3a8bc67
AbhishekPatil1310 <ap7497493@gmail.com>
2025-09-02 23:47:11 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ function buildApp() {
     'https://advestor-frontend.vercel.app',
       'https://patil-project.vercel.app',
     'https://vercel-frontend-eta-plum.vercel.app',
-    'https://www.advestors.org'
+    'https://www.advestors.org',
     'https://advestor-frontend.vercel.app'// ✅ your actual frontend
   ].filter(Boolean); // remove null/undefined
 
@@ -90,4 +90,5 @@ function buildApp() {
 
 
 module.exports = buildApp;
+
 
